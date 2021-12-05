8b15673ead56b3829de7eceb05017a8a5e5ca3da
tglgame <tglgame425@163.com>
2021-12-05 21:58:50 +0800

    update chapter8 httpserver

--- chapter8/server/httpserver.go
+++ chapter8/server/httpserver.go
@@ -2,26 +2,59 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"io"
-	"log"
 	"net"
 	"net/http"
 	"os"
 	"os/signal"
-	"sync"
 	"syscall"
 	"time"
+
+	rotatelogs "github.com/lestrrat-go/file-rotatelogs"
+	logger "github.com/sirupsen/logrus"
 )
 
-var logger *log.Logger
-var once sync.Once
+type configuration struct {
+	LogLevel int
+	Port     int
+	LogPath  string
+}
 
-func GetLogInstance() *log.Logger {
-	once.Do(func() {
-		logger = log.New(os.Stdout, "http_server:", log.Ldate|log.Ltime|log.Lshortfile)
-	})
-	return logger
+func ReadConfig() *configuration {
+	config_file := os.Getenv("CONFIG_FILE_PATH")
+	config_file = "conf.json"
+	file, _ := os.Open(config_file)
+	defer file.Close()
+
+	decorder := json.NewDecoder(file)
+	conf := configuration{}
+	err := decorder.Decode(&conf)
+	if err != nil {
+		logger.Error("Error read config file:", err)
+	}
+	return &conf
+}
+
+func Setlog(path string, LogLevel int) {
+	writer, _ := rotatelogs.New(
+		path+".%Y%m%d%H",
+		rotatelogs.WithMaxAge(time.Duration(24*60)*time.Hour),
+		rotatelogs.WithRotationTime(time.Duration(60)*time.Minute),
+	)
+
+	logger.SetOutput(writer)
+
+	if LogLevel == 1 {
+		logger.SetLevel(logger.DebugLevel)
+	} else if LogLevel == 2 {
+		logger.SetLevel(logger.InfoLevel)
+	} else if LogLevel == 3 {
+		logger.SetLevel(logger.WarnLevel)
+	} else if LogLevel == 4 {
+		logger.SetLevel(logger.ErrorLevel)
+	}
 }
 
 func GetRemoteIp(req *http.Request) string {
@@ -40,14 +73,12 @@ func GetRemoteIp(req *http.Request) string {
 }
 
 func HelloServer(w http.ResponseWriter, req *http.Request) {
-	logger := GetLogInstance()
-
-	logger.Println("====receive client header====")
+	logger.Info("====receive client header====")
 	clientIp := GetRemoteIp(req)
-	logger.Println("client addr: ", clientIp)
+	logger.Info("client addr: ", clientIp)
 
 	for k, v := range req.Header {
-		logger.Println("receive and set:", k, ":", v)
+		logger.Info("receive and set:", k, ":", v)
 		w.Header().Set(k, fmt.Sprintf("%s", v))
 	}
 
@@ -56,27 +87,31 @@ func HelloServer(w http.ResponseWriter, req *http.Request) {
 	w.Header().Set("VERSION", version)
 	w.Header().Set("servertag", "servervalue1")
 
-	logger.Println("return status code: ", 200)
+	logger.Info("return status code: ", 200)
 	w.WriteHeader(200)
 
-	logger.Println("=====================")
+	logger.Info("=====================")
 	io.WriteString(w, "hello, world!\n")
 }
 
 func Healthz(w http.ResponseWriter, req *http.Request) {
-	logger := GetLogInstance()
-	logger.Println("get health of server")
+	logger.Info("get health of server")
 	w.WriteHeader(200)
 }
 
 func main() {
-	logger := GetLogInstance()
+	conf := ReadConfig()
+	Setlog(conf.LogPath, conf.LogLevel)
+	logger.Info("**********************")
+	logger.Info(conf)
+	logger.Info(conf.LogPath, conf.LogLevel, conf.Port)
+	logger.Info("**********************")
 
 	mux := http.NewServeMux()
 	mux.HandleFunc("/hello", HelloServer)
 	mux.HandleFunc("/healthz", Healthz)
 	srv := http.Server{
-		Addr:    "0.0.0.0:80",
+		Addr:    fmt.Sprintf("0.0.0.0:%d", conf.Port),
 		Handler: mux,
 	}
 
@@ -89,16 +124,16 @@ func main() {
 			logger.Fatal("listen and serve: ", err)
 		}
 	}()
-	logger.Println("server started!")
+	logger.Info("server started!")
 	<-done
-	logger.Println("server stopped!")
+	logger.Info("server stopped!")
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer func() {
-		logger.Println("in cancel context...")
+		logger.Info("in cancel context...")
 		cancel()
 	}()
 	if err := srv.Shutdown(ctx); err != nil {
 		logger.Fatalln("Server shutdown failed: %+v", err)
 	}
-	logger.Println("Server gracefully stopped")
+	logger.Info("Server gracefully stopped")
 }
